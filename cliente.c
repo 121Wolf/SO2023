ac27f720f39817bc0dbd6c3ab92d663d8385be23
121Wolf <josehenrique1999@gmail.com>
2023-05-02 19:50:58 +0100

    logs working barely

--- cliente.c
+++ cliente.c
@@ -30,20 +30,3 @@ int main() {
 
     return 0;
 }
-
-#include <signal.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-#define MAX_SIZE 1024
-
-int main(int argc, char *argv[]){
-    char buffer[MAX_SIZE];
-    int fifo;
-    
-}
--- server.c
+++ server.c
@@ -9,29 +9,43 @@
 #define PIPE_NAME "my_pipe"
 
 int main() {
-    size_t fd;
+    size_t fdfifo, fd_log;
     char buffer[256];
     int bytes_read;
 
    // buffer = (char *) malloc(256);
     // create named pipe
     if(mkfifo(PIPE_NAME, 0666) == -1){
-        printf("Something is wrong");
+        perror("MKFIFO");
+    }
+    //open log.txt
+    if ((fd_log = open("log.txt", O_CREAT | O_TRUNC | O_WRONLY, 0666)) == -1) {
+        perror("Open log.txt");
+        return -1;
     }
 
     // open pipe for reading
-    fd = open(PIPE_NAME, O_RDONLY);
-
-    printf("Server listening... fd %ld \n",fd);
+    if((fdfifo = open(PIPE_NAME, O_RDONLY, 0666)) == -1){
+        perror("Open fifo");
+    }
+    else 
+        printf("[DEBUG] opened FIFO for reading\n");
+    //puts all values of buffer to \0
+    bzero(buffer, 256);
+    printf("[DEBUG] Server listening... fdfifo %ld \n",fdfifo);
 
     // read from pipe and print to console
-    while ((bytes_read = read(fd, buffer, sizeof(char))) > 0) {
+    while ((bytes_read = read(fdfifo, buffer, sizeof(buffer))) > 0) {
         printf("Received: %s\n", buffer);
     }
-
+        //write to log file the buffer message to save the command and arguments
+        write(fd_log,buffer,bytes_read);
+        write(fd_log, "\n", 1);
+        printf("[DEBUG] wrote %s to file\n", buffer);
     // close the pipe
-    close(fd);
+    close(fdfifo);
     printf("Pipe closed \n");
+    close(fd_log);
 
     // remove the named pipe
     unlink(PIPE_NAME);
