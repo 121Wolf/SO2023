29907f11e9685394f4eadf22f4c15742f6499a72
165Tripz <joao-porto2011@live.com>
2023-05-05 00:33:35 +0100

    Fixed

--- src/cliente.c
+++ src/cliente.c
@@ -16,24 +16,31 @@ int main() {
     int fd;
     char message[256];
     char userinput[256];
-    char *endmessage = "Coisas aconteceram bro";
-    int bytes_written, bytes_written2;
+    //char *endmessage = "Coisas aconteceram bro";
+    int bytes_written;
     int bytesread;
     char *command = "ls";
-    //int status = 0;
+    int status = 0;
 
 
      // open pipe for writing
     if((fd = open(PIPE_NAME, O_WRONLY)) == -1){
         perror("FIFO");
     }
-          if( bytesread = read(STDIN_FILENO,userinput,sizeof(userinput)) == -1){
+        if((bytesread = read(STDIN_FILENO,userinput,sizeof(userinput))) == -1){
           perror("Userinput:");
         }
-            commandparser(userinput,bytesread);
-        
+        //commandparser(userinput,bytesread);
+    else{
+        while(1){
+            if((bytesread = read(STDIN_FILENO,userinput,sizeof(userinput))) == -1){
+                perror("Userinput:");
+            }
+            //commandparser(userinput,bytesread);
+        }
+    }
 
-   //execl("/bin/ls", command, NULL);
+    //execl("/bin/ls", command, NULL);
     for (int i = 0; i < 5; i++) {
         // Guardar o pid do filho
         pid_t pid=fork();
@@ -43,18 +50,9 @@ int main() {
                 _exit(0);
             default:
                 printf("Fork successful %d\n",pid);
+                wait(&status);
         }
     }
-
-    else{
-        while(1){
-             if( bytesread = read(STDIN_FILENO,userinput,sizeof(userinput)) == -1){
-          perror("Userinput:");
-        }
-            commandparser(userinput,bytesread);
-        }
-    }
-
         // int to string, to be confirmed 
    /* long j;
     for (j = 1; j < pid ;j = j * 10);
@@ -76,13 +74,13 @@ int main() {
     printf("[DEBUG] Sent %d bytes to server\n", bytes_written);
     
     //waits for child PID _exit(0)
-    pid_t terminated_pid = wait(&status);
+    //pid_t terminated_pid = wait(&status);
     
     /*if(bytes_written2 = write(fd,endmessage,strlen(endmessage)) == -1){
         perror("A comunicação falhou devido:");
     }
     printf("[DEBUG] Sent %d bytes to server\n", bytes_written2);*/
-    }
+
     // close the pipe 
     close(fd);
 
