f4151c178692a24bac05ac0ddef13be29b8ca128
165Tripz <joao-porto2011@live.com>
2023-05-04 23:07:08 +0100

    Update

---
+++ src/cliente.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "assist.h"
+
+
+#define PIPE_NAME "my_pipe"
+
+int main() {
+    int fd;
+    char *message = "Hello, server!\n";
+    int bytes_written;
+    //char *command = "ls";
+
+
+    // open pipe for writing
+    fd = open(PIPE_NAME, O_WRONLY);
+    if(fd == -1) printf("pila");
+
+   //execl("/bin/ls", command, NULL);
+
+    // write message to pipe
+    bytes_written = write(fd, message, strlen(message) );
+
+    printf("Sent %d bytes to server\n", bytes_written);
+
+    // close the pipe
+    close(fd);
+
+    return 0;
+}
