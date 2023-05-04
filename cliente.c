915ec0d21367a202b35e78b6f1f4bb5e78acfda8
121Wolf <josehenrique1999@gmail.com>
2023-05-04 23:34:38 +0100

    giwgijgi

--- cliente.c
+++ cliente.c
@@ -10,22 +10,69 @@
 
 int main() {
     int fd;
-    char *message = "Hello, server!\n";
-    int bytes_written;
+    char message[256];
+    char userinput[256];
+    char *endmessage = "Coisas aconteceram bro";
+    int bytes_written, bytes_written2;
+    int bytesread;
     char *command = "ls";
+    pid_t pid;
+    int status;
 
-    // open pipe for writing
-    fd = open(PIPE_NAME, O_WRONLY);
-    if(fd == -1) printf("pila");
+     // open pipe for writing
+    if((fd = open(PIPE_NAME, O_WRONLY)) == -1){
+        perror("FIFO");
+    }
+          if( bytesread = read(STDIN_FILENO,userinput,sizeof(userinput)) == -1){
+          perror("Userinput:");
+        }
+            commandparser(userinput,bytesread);
+        
 
-   //execl("/bin/ls", command, NULL);
+    if ((pid = fork()) == 0) {
+        printf("This is child PID speaking");
+		//execl("/bin/ls", "/bin/ls", "-l", NULL);
+		_exit(0);
+}
 
+    else{
+        while(1){
+             if( bytesread = read(STDIN_FILENO,userinput,sizeof(userinput)) == -1){
+          perror("Userinput:");
+        }
+            commandparser(userinput,bytesread);
+        }
+    }
+
+        // int to string, to be confirmed 
+   /* long j;
+    for (j = 1; j < pid ;j = j * 10);
+    j = j / 10;
+    int i;
+    for (i = 0; j != 0; i++) {
+        message[i] = (pid/j) + '0';
+        pid = pid % j;
+        j = j / 10;
+    }
+    
+    message[i] = 0;
+*/
     // write message to pipe
-    bytes_written = write(fd, message, strlen(message) );
-
-    printf("Sent %d bytes to server\n", bytes_written);
-
-    // close the pipe
+    if((bytes_written = write(fd, message, strlen(message))) == -1){
+        perror("FIFO Write:");
+    }
+    
+    printf("[DEBUG] Sent %d bytes to server\n", bytes_written);
+    
+    //waits for child PID _exit(0)
+    pid_t terminated_pid = wait(&status);
+    
+    /*if(bytes_written2 = write(fd,endmessage,strlen(endmessage)) == -1){
+        perror("A comunicação falhou devido:");
+    }
+    printf("[DEBUG] Sent %d bytes to server\n", bytes_written2);*/
+    }
+    // close the pipe 
     close(fd);
 
     return 0;
--- server.c
+++ server.c
@@ -13,7 +13,8 @@ int main() {
     char buffer[256];
     int bytes_read;
     int bytes_wrote;
-    char *message = "mete isto";;
+    char *message = "mete isto";
+    pid_t memory[256];
 
    // buffer = (char *) malloc(256);
     // create named pipe
@@ -26,6 +27,7 @@ int main() {
         return -1;
     }
 
+
     // open pipe for reading
     if((fdfifo = open(PIPE_NAME, O_RDONLY, 0666)) == -1){
         perror("Open fifo");
@@ -33,26 +35,33 @@ int main() {
     else 
         printf("[DEBUG] opened FIFO for reading\n");
     //puts all values of buffer to \0
-    bzero(buffer, 256);
-    printf("[DEBUG] Server listening... fdfifo %ld \n",fdfifo);
+        bzero(buffer, 256);
+        printf("[DEBUG] Server listening... fdfifo %lu \n",fdfifo);
 
-    // read from pipe and print to console
-    size_t readen = 0;
-    while ((bytes_read = read(fdfifo, buffer, sizeof(buffer))) > 0) {
-        readen += bytes_read;
-        printf("Received: %s\n and %lu", buffer, readen);
-    }
+    
+     // read from pipe and print to console
+        size_t readen = 0;
+        while ((bytes_read = read(fdfifo, buffer, sizeof(buffer))) > 0) {
+            readen += bytes_read;
+            printf("Received: %s and  read %lu bytes", buffer, readen);
+        }
+        int pid=0;
+        for (int i = 0;i < readen; i++) {
+            pid = pid*10;
+            pid+=buffer[i]-'0';
+        }
+
+        printf("Este é o PID do filho: %d\n", pid);
+
         //write to log file the buffer message to save the command and arguments
-       if ((bytes_wrote = write(fd_log,buffer,readen)) == -1) {
-        printf("[DEBUG] wrote %s to file\n", buffer);
-    } 
+        //if ((bytes_wrote = write(fd_log,buffer,readen)) == -1) {
+        //    printf("[DEBUG] wrote %s to file\n", buffer);
+    
     
     // close the pipe
     close(fdfifo);
     printf("Pipe closed \n");
     close(fd_log);
-
-    // remove the named pipe
 
     return 0;
 }
