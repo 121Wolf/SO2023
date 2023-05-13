0dbc3c720266c46b2fdf28cade5fb4e5c10e56b0
165Tripz <joao-porto2011@live.com>
2023-05-13 18:37:04 +0100

    mensagem

--- src/cliente.c
+++ src/cliente.c
@@ -12,7 +12,6 @@
 
 
 #define PIPE_NAME "my_pipe"
-int liveness = 1;
 
 
 struct Node pid_status;// here we put the pids of child that have not finished yet
@@ -20,11 +19,12 @@ struct Node *head = NULL;
 
 
 int parserinput(char * userinput){
+    strtok(userinput,"\n");
     char* firstWord = strtok(userinput," ");
-    if (strcmp(userinput,"status\n") == 0)
+    if (strcmp(userinput,"status") == 0)
         return 2;
-    else if (strcmp(userinput,"exit\n") == 0)
-        return 3;
+    else if (strcmp(userinput,"exit") == 0)
+        return 0;
     else if (strcmp(firstWord,"execute") == 0) {
         char* secondWord = strtok(NULL," ");
         if (strcmp(secondWord,"-u") == 0)
@@ -37,49 +37,52 @@ int main() {
     int fd; // the file descripter of my_pipe 
     char userinput[256]; // user inputs on the terminal
     pid_t pid_worker[1]; //buffer that hold the pid of a process 
-    int bytes_written, bytesread;
+    int bytes_written = 0, bytesread = 0;
     //int counter = 0; //counts the number of active processes in pid_status
     int pipefd[2]; // file descriptors of a pipe that reads from processes and is written by other childs
     int status = 0;
-    int parser = 0;
+    int parser = 1;
 
     //create pipe
     if (pipe(pipefd) == -1) {
         perror("pipe");
         exit(1);
     }
+
     pid_t processes = fork();
 
     if (processes == 0) {
         close(pipefd[1]); //close write end
-        while(1) {
-            if (read(pipefd[0], pid_worker, sizeof(pid_t)) == -1) perror("read");
+        while(read(pipefd[0], pid_worker, sizeof(pid_t))) {
             printf("[DEBUG] PID RECEBIDO %d \n",pid_worker[0]);
             checkList(head,pid_worker[0]);
         }
+        close(pipefd[0]);
     } else {
-        while(liveness) {
-            printf("[DEBUG] Insert command: \n");
+        close(pipefd[0]);
+        if((fd = open(PIPE_NAME, O_WRONLY)) == -1) perror("FIFO\n");
+        while(parser) {
+            write(STDOUT_FILENO,"[DEBUG] Insert command: ",25);
             if((bytesread = read(STDIN_FILENO,userinput,sizeof(userinput))) == -1) perror("Userinput:");
 
             //passes the input  
+
             parser = parserinput(userinput);
+            
             pid_t pidcontrol=fork();
             
             //the child process shall process the input command and the parent shall wait for more user inputs
             if (pidcontrol == 0){
                 switch (parser) {
                     case 0:
-                        printf("[DEBUG] This input cannot be processed, try again");
+                        printf("\nO programa vai fechar\n");
                         break;
-
                     case 1: //execute -u 
                         struct timeval time[3];
                         printf("[DEBUG] This executes the execl function \n");
-                        if((fd = open(PIPE_NAME, O_WRONLY)) == -1) perror("FIFO");
-                        if((bytes_written = write(fd, userinput,strlen(userinput))) == -1) perror("FIFO Write:");
+                        if((bytes_written = write(fd, userinput,strlen(userinput))) == -1) perror("FIFO Write:\n");
                         gettimeofday(time,0);
-                        if((bytes_written = write(fd, time,sizeof(struct timeval))) == -1) perror("FIFO Write:");
+                        if((bytes_written = write(fd, time,sizeof(struct timeval))) == -1) perror("FIFO Write:\n");
                         printf("[DEBUG] This reaches the execl function \n");
                         pid_t pid=fork();
                         switch(pid) {
@@ -93,19 +96,8 @@ int main() {
                                     exit(1);
                                 }
                                 close(pipefd[1]);
-                                strtok(userinput," ");
-                                strtok(NULL," ");
-                                char* word = strtok(NULL," ");
-                                execl(word,word,NULL);
+                                execl("/bin/ps","ps",NULL);
                             default:
-                                //writes the pid in the array of unfinished child pids, to be used for status
-                                /* counter = 0;                   
-                                for(int i = 0;i<10;i++){
-                                    if(pid_status != 0){
-                                        counter ++;
-                                    }
-                                    pid_status[counter++] = pid;
-                                    }*/
                                 printf("[DEBUG] Este é o pid do processo filho %d \n",pid);
                                 printf("[DEBUG] Este é o pid do processo pai %d \n",getpid());
                                 pid_worker[0] = pid;
@@ -113,7 +105,7 @@ int main() {
                                 close(pipefd[0]);
                                 if (write(pipefd[1], pid_worker, sizeof(pid_t)) == -1) {
                                     perror("cannot write to father");
-                                    exit(1);
+                                    _exit(1);
                                 }
                                 close(pipefd[1]);
                                 gettimeofday(time+1,NULL);
@@ -127,60 +119,33 @@ int main() {
                                     time[2].tv_sec--;
                                     time[2].tv_usec += 1000000;
                                 }
-                                printf("[DEBUG] The seconds %ld and the microseconds %ld \n",time[2].tv_sec,time[2].tv_usec);       
+                                printf("\n[DEBUG] The seconds %ld and the microseconds %ld \n",time[2].tv_sec,time[2].tv_usec);
                                 break;
                             }
+                        close(fd);
+                        _exit(0);
                         break;
                     case 2:
-                        printf("[DEBUG] This returns the status \n");
+                        printf("\n[DEBUG] This returns the status \n");
                         //code here
-                        break;
-                    case 3:
-                        printf("[DEBUG] This quits the program\n");
-                        liveness = 0;
+                        _exit(0);
                         break;
                     default:
-                        printf("[DEBUG] Unknown command\n");
+                        printf("\n[DEBUG] Unknown command\n");
+                        _exit(0);
                         break;
                 }
+                bzero(userinput,sizeof(userinput));
                 printf("[DEBUG] Sent %d bytes to server\n", bytes_written);
-                //waits for child PID _exit(0)
-                //pid_t terminated_pid = wait(&status);
             } else {
-                /* close(pipefd[1]); //close write end
-                if (read(pipefd[0], &pid_worker, sizeof(pid_t)) == -1) {
-                    perror("read");
-                    exit(1);
-                }
-                insertAtBeginning(&head, pid_worker); //this adds pid of child to linked list of processes that have yet not ended
-                printf("Child PID received: %d\n", pid_worker);
-                close(pipefd[0]);
-                */
                 printf("Fork successful \n");
-                //  for(int i=0;i < counter;i++){
-                //      printf("The child  %d \n",pid_status[i]);
-                //  }
             }
         }
-        // close the pipe 
+        close(pipefd[0]);
         close(fd);
+        // close the pipe 
         return 0;
     }         
     
 }
 
-   
-    //}
-        // int to string, to be confirmed 
-   /* long j;
-    for (j = 1; j < pid ;j = j * 10);
-    j = j / 10;
-    int i;
-    for (i = 0; j != 0; i++) {
-        message[i] = (pid/j) + '0';
-        pid = pid % j;
-        j = j / 10;
-    }
-    
-    message[i] = 0;
-*/
