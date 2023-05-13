cf9c6303b447e23bb4cbc96b43e092ffed79646b
165Tripz <joao-porto2011@live.com>
2023-05-14 00:37:21 +0100

    asd

--- src/cliente.c
+++ src/cliente.c
@@ -12,41 +12,7 @@
 
 #define PIPE_NAME "my_pipe"
 
-
-
-
-/*
-
-dif = 0x001
-pid = 0x001
-
-pidl = 0x002
-
-*pid = [nome*4,pid*4,next*4]
-
-*pid -> nome = 0x111
-
-**pid.pif = 1234
-**pid.nome = "ps"
-**pid.next = 0x002
-
-struct timeval time[100];
-pid_t pid[100];
-int counter= 0; 
-
-while(head !=NULL){
-    counter++;
-    pid = head->pid ;
-    time= head->time;
-}
-
-*/
-
-
-
-
-
-int parserinput(char * userinput){
+int parserinput(char* userinput) {
     strtok(userinput,"\n");
     char* firstWord = strtok(userinput," ");
     if (strcmp(userinput,"status") == 0)
@@ -66,115 +32,76 @@ int main() {
     int fd_status; //fd of status pipe
     char userinput[256]; // user inputs on the terminal
     char buffer[256]; // 
-    pid_t pid_worker[1]; //buffer that hold the pid of a process 
     int bytes_written = 0, bytesread = 0;
-    //int counter = 0; //counts the number of active processes in pid_status
-    int pipefd[2]; // file descriptors of a pipe that reads from processes and is written by other childs
     int status = 0;
     int parser = 1;
-
-    //create pipe
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        exit(1);
-    }
     
-    pid_t processes = fork();
-
-    if (processes == 0) {
-        close(pipefd[1]); //close write end
-        while(read(pipefd[0], pid_worker, sizeof(pid_t))) {
-            printf("[DEBUG] PID RECEBIDO %d \n",pid_worker[0]);
-         //   checkList(head,pid_worker[0]);
-        }
-        close(pipefd[0]);
-    } else {
-        close(pipefd[0]);
-        if((fd = open(PIPE_NAME, O_WRONLY)) == -1) perror("FIFO\n");
-        while(parser) {
-            write(STDOUT_FILENO,"[DEBUG] Insert command: ",25);
-            if((bytesread = read(STDIN_FILENO,userinput,sizeof(userinput))) == -1) perror("Userinput:");
-
-            //passes the input  
-
-            parser = parserinput(userinput);
-            
-            pid_t pidcontrol=fork();
-            
-            write(STDOUT_FILENO,outputbuff,)
-            //the child process shall process the input command and the parent shall wait for more user inputs
-            if (pidcontrol == 0){
-                switch (parser) {
-                    case 0:
-                        printf("\nO programa vai fechar\n");
-                        break;
-                    case 1: //execute -u 
-                        struct timeval time[3];
-                        printf("[DEBUG] This executes the execl function \n");
-                        if((bytes_written = write(fd, userinput,strlen(userinput))) == -1) perror("FIFO Write:\n");
-                        gettimeofday(time,0);
-                        if((bytes_written = write(fd, time,sizeof(struct timeval))) == -1) perror("FIFO Write:\n");
-                        printf("[DEBUG] This reaches the execl function \n");
-                        pid_t pid=fork();
-                        switch(pid) {
-                            case 0: 
-                                printf("[DEBUG] I AM ALIVE \n");
-                                close(pipefd[1]);
-                                execl("/bin/ps","ps",NULL);
-                                break;
-                            default:
-                                printf("[DEBUG] Este é o pid do processo filho %d \n",pid);
-                                printf("[DEBUG] Este é o pid do processo pai %d \n",getpid());
-                                pid_worker[0] = pid;
-                                wait(&status);
-                                kill(pid, SIGKILL);
-                                gettimeofday(time+1,NULL);
-                                if((bytes_written = write(fd    , time+1,sizeof(struct timeval))) == -1){
-                                    perror("FIFO Write:");
-                                }
-                                time[2].tv_sec = time[1].tv_sec - time[0].tv_sec;
-                                time[2].tv_usec = time[1].tv_usec - time[0].tv_usec;
-                                //normalizes the diffrence
-                                if (time[2].tv_usec < 0) {
-                                    time[2].tv_sec--;
-                                    time[2].tv_usec += 1000000;
-                                }
-                                printf("\n[DEBUG] The seconds %ld and the microseconds %ld \n",time[2].tv_sec,time[2].tv_usec);
-                                close(fd);
-                                _exit(0);
-                                break;
+    if((fd = open(PIPE_NAME, O_WRONLY)) == -1) perror("FIFO\n");
+    while(parser) {
+        if((bytesread = read(STDIN_FILENO,userinput,sizeof(userinput))) == -1) perror("Userinput:");
+        //passes the input  
+        char temp[265];
+        strcpy(temp,userinput);
+        parser = parserinput(userinput);
+        
+        pid_t pidcontrol=fork();
+        
+        //the child process shall process the input command and the parent shall wait for more user inputs
+        if (pidcontrol == 0){
+            switch (parser) {
+                case 0:
+                    break;
+                case 1: //execute -u 
+                    struct timeval time[3];
+                    if((bytes_written = write(fd, temp,strlen(temp)))) == -1) perror("FIFO Write:\n");
+                    gettimeofday(time,0);
+                    if((bytes_written = write(fd, time,sizeof(struct timeval))) == -1) perror("FIFO Write:\n");
+                    pid_t pid=fork();
+                    switch(pid) {
+                        case 0: 
+                            int i = 0;
+                            char *args[64];
+                            while ((args[i++] = strtok(NULL," ")) != NULL);
+                            execvp(args[0],args);
+                            break;
+                        default:
+                            wait(&status);
+                            gettimeofday(time+1,NULL);
+                            if((bytes_written = write(fd, time+1,sizeof(struct timeval))) == -1){
+                                perror("FIFO Write:");
                             }
-                        break;
-                    case 2:
-                        printf("\n[DEBUG] This returns the status \n");
-                         if((fd_status = open("status_pipe", O_RDONLY, 0666)) == -1){
-                          perror("Open fifo");
-                }
-                        if((bytes_written = write(fd, userinput,strlen(userinput))) == -1) perror("FIFO Write:\n");
-                        bzero(buffer, 256);
-                        char statusString[300];
-                        while ((bytesread = read(fd_status, buffer, sizeof(buffer))) > 0) {
-                                 printf("Received: %s  \n", buffer);
-        }               
-                        showPIDS(statusString); //Reveals to the user the pids, names and times of active processes
-                        _exit(0);
-                        break;
-                    default:
-                        printf("\n[DEBUG] Unknown command\n");
-                        _exit(0);
-                        break;
-                }
-                bzero(userinput,sizeof(userinput));
-                printf("[DEBUG] Sent %d bytes to server\n", bytes_written);
-            } else {
-                printf("Fork successful \n");
+                            time[2].tv_sec = time[1].tv_sec - time[0].tv_sec;
+                            time[2].tv_usec = time[1].tv_usec - time[0].tv_usec;
+                            //normalizes the diffrence
+                            if (time[2].tv_usec < 0) {
+                                time[2].tv_sec--;
+                                time[2].tv_usec += 1000000;
+                            }
+                            _exit(0);
+                            break;
+                        }
+                    break;
+                case 2:
+                    if((fd_status = open("status_pipe", O_RDONLY, 0666)) == -1){
+                        perror("Open fifo");
+                    }
+                    if ((bytes_written = write(fd, "status",7)) == -1) perror("FIFO Write:\n");
+                    bzero(buffer, 256);
+                    while ((bytesread = read(fd_status, buffer, sizeof(buffer))) > 0) {
+                             printf("Received: %s \n", buffer);
+                    }               
+                    //showPIDS(statusString); //Reveals to the user the pids, names and times of active processes
+                    _exit(0);
+                    break;
+                default:
+                    _exit(0);
+                    break;
             }
+            bzero(userinput,sizeof(userinput));
         }
-        close(pipefd[0]);
-        close(fd);
-        // close the pipe 
-        return 0;
-    }         
-    
+    }
+    close(fd);
+    // close the pipe 
+    return 0;        
 }
 
--- src/server.c
+++ src/server.c
@@ -25,13 +25,18 @@ int parserinput(char * userinput){
     return -1;
 }
 
+void fullparser(char * string, struct timeval * timestamp, pid_t *pid, char * name) {
+    timestamp = malloc(sizeof(struct timeval));
+    *pid = 1;
+    name = string;
+}
+
  struct Node pid_status;// here we put the pids of child that have not finished yet
 struct Node *head = NULL;
 
 
-
 int main() {
-    //int pipefd[2];
+    int pipefd[2];
     //pid_t pid_worker[1];
     pid_t pid_fire;
     size_t fdfifo, fd_log,fd_status;
@@ -72,73 +77,66 @@ int main() {
     }
     printf("[DEBUG] opened FIFO for reading\n");
     pid_t processes = fork();
-         if(processes == 0){
-            close(pipefd[1]); //close write end
-                while(1){
-                    if (read(pipefd[0], ctrlbuf, sizeof(ctrlbuf)) == -1) {
-                        perror("read");
-                        }
-                struct timeval tempo,diff;
-                pid_t newpid;
+    if(processes == 0){
+        close(pipefd[1]); //close write end
+            while(1){
+                if (read(pipefd[0], ctrlbuf, sizeof(ctrlbuf)) == -1) perror("read");
+                struct timeval *tempo = NULL,diff;
+                pid_t *newpid = 0;
                 char newname[50];
                 struct Node* spain= NULL;
                 fullparser(ctrlbuf,tempo,newpid, newname);
-                printf("[DEBUG] PID %d TEMPO %ld nome %s \n", newpid,time.tv_sec,newname);
-                if((spain=pidis(head,newpid))!= NULL){ //percorre e encontra um pid igual na lista ligada 
-                                         //faz a contagem do tempo
-                                        //meter o processo no log file
-                                        //retira o node com este pid
-
-                                diff.tv_sec = tempo.tv_sec - spain->time.tv_sec;
-                                diff.tv_usec = tempo.tv_usec - spain->time.tv_usec;
-                                //normalizes the diffrence
-                                if (diff.tv_usec < 0) {
-                                    diff.tv_sec--;
-                                    diff.tv_usec += 1000000;
-                                }
-                                printf("[DEBUG] The seconds %ld and the microseconds %ld \n",diff.tv_sec,diff.tv_usec); 
-                                //escrever no log     NEED TO BE DONE !!!!!!!!!! 
-                                break;
-
-                }     
-                else{
+                printf("[DEBUG] PID %d TEMPO %ld nome %s \n",*newpid,tempo->tv_sec,newname);
+                if((spain=pidIs(head,*newpid))!= NULL){                                                  //percorre e encontra um pid igual na lista ligada 
+                                                                                                        //faz a contagem do tempo
+                                                                                                        //meter o processo no log file
+                                                                                                        //retira o node com este pid
+                            diff.tv_sec = tempo->tv_sec - spain->time.tv_sec;
+                            diff.tv_usec = tempo->tv_usec - spain->time.tv_usec;
+                            //normalizes the diffrence
+                            if (diff.tv_usec < 0) {
+                                diff.tv_sec--;
+                                diff.tv_usec += 1000000;
+                            }
+                            printf("[DEBUG] The seconds %ld and the microseconds %ld \n",diff.tv_sec,diff.tv_usec); 
+                            //escrever no log     NEED TO BE DONE !!!!!!!!!! 
+                            break;
+                }else{
                     //adds the new process to the begining of the list
-                    if(checkList(head,newpid,newname,tempo) == 0);
-                    printf("[DEBUG] NODE ADDED TO LIST OF ACTIVE PROCESSES");
+                    if(checkList(head,*newpid,newname,*tempo) == 0)
+                        printf("[DEBUG] NODE ADDED TO LIST OF ACTIVE PROCESSES");
                     else printf("[DEBUG] Something is wrong I can feel it");
                 }       
-               printf("[DEBUG] LIST OF PROCESSES UPDATE, will read again");
+                printf("[DEBUG] LIST OF PROCESSES UPDATE, will read again");
             }
-            close(pipefd[0]);
-     }
+        close(pipefd[0]);
+    }
 
      else{
     //puts all values of buffer to \0
-       while(1){
+    while(1){
         bzero(buffer, 256);
         printf("[DEBUG] Server listening... fdfifo %lu \n",fdfifo);
      // read from pipe and print to console
         size_t readen = 0;
-        while ((bytes_read = read(fdfifo, buffer, sizeof(buffer))) > 0) {
-            readen += bytes_read;
-            printf("Received: %s and  read %lu bytes \n", buffer, readen);
-        }
+        bytes_read = read(fdfifo, buffer, sizeof(buffer));
+        printf("Received: %s and  read %i bytes \n", buffer, bytes_read);
         fire = parserinput(buffer); //will parse the command that was given by the client
         printf("Este é o Caso do parser: %d\n", fire);
         pid_fire = fork();
         if(pid_fire == 0){
-        switch (fire)
-        {
-        case 1: //this is the execute case
+        switch (fire){
+            case 1: //this is the execute case
                close(pipefd[0]);
                 if (write(pipefd[1], buffer, readen) == -1) {
-                     perror("cannot write to pid controller");
+                    perror("cannot write to pid controller");
                     exit(1);
                 }
                 close(pipefd[1]);
-            break;
+                break;
         
-        case 2: //this is status case
+            case 2: //this is status case
+                printf("Received status\n");
                 pid_t activepids[50];
                 char *name[50];
                 struct timeval stop[50];
@@ -154,27 +152,26 @@ int main() {
                     counter++;
               } 
                 struct timeval end,diff[50];
-                gettimeofday(end,NULL);
+                gettimeofday(&end,NULL);
                 for(int i = 0; i <= counter;i++){
-                 diff[i].tv_sec = end.tv_sec - stop[i].tv_sec;
-                 diff[i].tv_usec = end.tv_usec - stop[i].tv_usec;
-                                //normalizes the diffrence
-                                if (diff[i].tv_usec < 0) {
-                                    diff[i].tv_sec--;
-                                    diff[i].tv_usec += 1000000;
-                                }
-                                printf("[DEBUG] The seconds %ld and the microseconds %ld \n",diff[i].tv_sec,diff[i].tv_usec);
+                    diff[i].tv_sec = end.tv_sec - stop[i].tv_sec;
+                    diff[i].tv_usec = end.tv_usec - stop[i].tv_usec;
+                    //normalizes the diffrence
+                    if (diff[i].tv_usec < 0) {
+                        diff[i].tv_sec--;
+                        diff[i].tv_usec += 1000000;
+                    }
+                    printf("[DEBUG] The seconds %ld and the microseconds %ld \n",diff[i].tv_sec,diff[i].tv_usec);
                 }
-              
-                 statusString = statusparser(activepids,&name,stop,counter+1);//will parse informartion into a string
-                 if((bytes_written = write(fd_status, statusString,strlen(statusString))) == -1) perror("FIFO Write:\n");
 
-           break;
+                dataToString(activepids,name,stop,counter+1,statusString);//will parse informartion into a string
+                if((bytes_written = write(fd_status, statusString,strlen(statusString))) == -1) perror("FIFO Write:\n");
+                break;
 
-        default: //this is unkown command case
-            printf("[DEBUG] Error! Command unknown");
-            break;
-            _exit(0);
+            default:
+                printf("[DEBUG] Error! Command unknown");
+                _exit(0);
+                break;
         }
 
             _exit(0);
@@ -189,27 +186,3 @@ int main() {
 }
 }
 
-
-
-        /* this is something idk
-        int pid=0;
-        for (int i = 0;i < readen; i++) {
-            pid = pid*10;
-            pid+=buffer[i]-'0';
-        }
-        */
-        
-
-        //write to log file the buffer message to save the command and arguments
-        //if ((bytes_wrote = write(fd_log,buffer,readen)) == -1) {
-        //    printf("[DEBUG] wrote %s to file\n", buffer);
-    
-    
-    // close the pipe
-    close(fdfifo);
-    printf("Pipe closed \n");
-    close(fd_log);
-
-    return 0;
-}
-}
